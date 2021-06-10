4e817853685d8295c8203ecd4f61e99b789a5004
James Wiryo <jamesgiggs95@gmail.com>
2021-06-10 16:39:22 +0800

    Starting out new test app

---
+++ uitest/uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by James Denny Wiryo on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
